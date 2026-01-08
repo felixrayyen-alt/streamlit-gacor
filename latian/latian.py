000c2116e170e317b617c4c3a0fe031ecce6895a
felixrayyen-alt <felixrayyen@gmail.com>
2026-01-08 21:09:05 +0700

    Initial commit

---
+++ latian/latian.py
@@ -0,0 +1,84 @@
+import streamlit as st
+
+# Hitung Luas
+def luas_segitiga(a, t):
+     return (a * t) / 2
+def luas_persegi_panjang (p, l):
+    return p * l
+def luas_jajar_genjang(a, t):
+    return a * t
+
+# Hitung Keliling
+def keliling_segitiga(a, b, c):
+    return a + b + c
+def keliling_persegi_panjang (p, l):
+    return 2 * (p+l)
+def keliling_jajar_genjang(a, b):
+    return 2 * (a+b)
+
+hitungluas = {
+    "Luas Segitiga": {
+    "Fungsi": luas_segitiga,
+    "Inputan": ['Alas',"Tinggi" ] 
+},
+    "Luas Persegi panjang": {
+        "Fungsi": luas_persegi_panjang,
+        "Inputan": ['Panjang', 'Lebar']
+},
+    "Luas jajar genjang": {
+        "Fungsi": luas_jajar_genjang,
+        "Inputan": ['Alas', 'Tinggi']
+
+}
+
+}
+
+
+hitungkeliling = {
+    "Keliling Segitiga": {
+        "Fungsi": keliling_segitiga,
+        "Inputan": ['Sisi A', 'Sisi B', 'Sisi c']
+},
+
+    "Keliling Persegi Panjang": {
+        "Fungsi": keliling_persegi_panjang,
+        "Inputan": ['Panjang', 'Lebar']
+},
+    "Keliling Jajar genjang": {
+        "Fungsi": keliling_jajar_genjang,
+        "Inputan": ['Sisi A', 'Sisi B']
+
+}
+}
+
+st.title("Aplikasi Hitung Bangun Datar")
+
+opt = st.selectbox(
+    label="Pilih operasih perhitungan",
+    options=['Hitung Luas', 'Hitung keliling']
+)
+
+def pilih_rumus(option):
+    allRumus = {}
+
+    if (option == 'Hitung Luas'):
+        allRumus = hitungluas
+    else:
+        allRumus = hitungkeliling
+
+    return allRumus
+
+all_rumus = pilih_rumus(opt)
+
+pilih_hitung = st.radio(
+    label='Pilih Hitung' ,
+    options=all_rumus.keys(),
+    horizontal=True
+)
+
+inputs = [st.number_input(label, value=0.0) for label in all_rumus[pilih_hitung]["Inputan"]]
+
+if st.button('Hitung'):
+    hasil = all_rumus[pilih_hitung]["Fungsi"](*inputs)
+    st.markdown(f'<h2 style="color:green; text-align:center;>Hasil: {hasil}</h2>', 
+                unsafe_allow_html=True)
